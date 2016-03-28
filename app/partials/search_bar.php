d95af6fcb315bfd76abba826bf5b5a1afa104b3f
Ben <benjheng@gmail.com>
2016-03-28 12:44:27 +0800

    Persist search query in search bar

--- app/partials/search_bar.php
+++ app/partials/search_bar.php
@@ -1,8 +1,35 @@
+<?php
+    $searchQuery = '';
+    $minBid = '';
+    $maxBid = '';
+    $type = '';
+    $minQuantity = '';
+    $location = '';
+
+    if (isset($_GET['search-query'])) {
+        $searchQuery = htmlspecialchars($_GET['search-query']);
+    }
+    if (isset($_GET['min-bid'])) {
+        $minBid = htmlspecialchars($_GET['min-bid']);
+    }
+    if (isset($_GET['max-bid'])) {
+        $maxBid = htmlspecialchars($_GET['max-bid']);
+    }
+    if (isset($_GET['type'])) {
+        $type = htmlspecialchars($_GET['type']);
+    }
+    if (isset($_GET['min-quantity'])) {
+        $minQuantity = htmlspecialchars($_GET['min-quantity']);
+    }
+    if (isset($_GET['location'])) {
+        $location = htmlspecialchars($_GET['location']);
+    }
+?>
 <form class="container-fluid search-bar panel panel-default">
     <div class='panel-body'>
         <div class='main-search-wrapper'>
             <div class="input-group main-search">
-                <input type="text" name='search-query' class="form-control" placeholder="Item name or description" autofocus/>
+            <input type="text" name='search-query' class="form-control" placeholder="Item name or description" value="<?php echo $searchQuery; ?>" autofocus/>
                 <span class="input-group-btn">
                     <button class="btn btn-default" name='search-submit' type="submit">Search!</button>
                 </span>
@@ -15,34 +42,34 @@
             <div class='row'>
                 <div class="input-group col-sm-4">
                     <span class="input-group-addon">Min bid</span>
-                    <input type="number" name='min-bid' class="form-control" min='0'>
+                    <input type="number" name='min-bid' value="<?php echo $minBid; ?>" class="form-control" min='0'>
                 </div>
                 <div class="input-group col-sm-4">
                     <span class="input-group-addon">Max bid</span>
-                    <input type="number" name='max-bid' class="form-control" min='0'>
+                    <input type="number" name='max-bid' class="form-control" value="<?php echo $maxBid; ?>" min='0'>
                 </div>
                 <div class="input-group col-sm-4">
                     <span class="input-group-addon">Type</span>
                     <select class='selectpicker form-control' name='type'>
                         <option selected>All</option>
-                        <option>Appliance</option>
-                        <option>Book</option>
-                        <option>Furniture</option>
-                        <option>Tool</option>
-                        <option>Others</option>
+                        <option <?php echo $type == 'Appliance' ? 'selected' : '' ?>>Appliance</option>
+                        <option <?php echo $type == 'Book' ? 'selected' : '' ?>>Book</option>
+                        <option <?php echo $type == 'Furniture' ? 'selected' : '' ?>>Furniture</option>
+                        <option <?php echo $type == 'Tool' ? 'selected' : '' ?>>Tool</option>
+                        <option <?php echo $type == 'Others' ? 'selected' : '' ?>>Others</option>
                     </select>
                 </div>
                 <div class="input-group col-sm-4">
                     <span class="input-group-addon">Min quantity</span>
-                    <input type="number" name='min-quantity' class="form-control" min='0'>
+                    <input type="number" name='min-quantity' value="<?php echo $minQuantity; ?>" class="form-control" min='0'>
                 </div>
                 <div class="input-group col-sm-4">
                     <span class="input-group-addon">Max buyout</span>
-                    <input type="number" name='max-buyout' class="form-control" min='0'>
+                    <input type="number" name='max-buyout' class="form-control" value="<?php echo $maxBuyout; ?>" min='0'>
                 </div>
                 <div class="input-group col-sm-12">
                     <span class="input-group-addon">Location</span>
-                    <input type="text" name='location' class="form-control" min='0'>
+                    <input type="text" name='location' class="form-control" value="<?php echo $location ?>" min='0'>
                 </div>
             </div>
         </div>
