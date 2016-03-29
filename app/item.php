74b4f5b0810565b6662b94a744682d3007b37ba2
Ben <benjheng@gmail.com>
2016-03-30 00:12:31 +0800

    Fixes #24

--- app/item.php
+++ app/item.php
@@ -8,7 +8,9 @@ $dbconn = pg_connect("host=localhost port=5432 dbname=stuffy_db user=postgres
 or die('Could not connect: ' . pg_last_error());
 ?>
 <?php
-session_start();
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 $owner = '';
 $item_name = '';
 $type = '';
@@ -16,6 +18,7 @@ $description = '';
 $starting_bid = '';
 $bid_deadline = '';
 $buyout = '';
+$buyout_int = '';
 $quantity = '';
 $pickup_location = '';
 $return_location = '';
@@ -40,7 +43,8 @@ if (pg_num_rows($result) > 0) {
     $description = $row['description'] == '' ? 'None' : $row['description'];
     $starting_bid = '$' . $row['starting_bid'];
     $bid_deadline = $row['bid_deadline'];
-    $buyout = $row['buyout'] == NULL ? 'None' : '$' . $row['buyout'];
+    $buyout_int = $row['buyout'];
+    $buyout = $buyout_int == NULL ? 'None' : '$' . $buyout_int;
     $quantity = $row['available_quantity'];
     $pickup_location = $row['pickup_location'] == '' ? 'None' : $row['pickup_location'];
     $return_location = $row['return_location'] == '' ? 'None' : $row['return_location'];
@@ -53,6 +57,7 @@ $query = "SELECT MAX(bid) AS bid FROM bid WHERE owner = $1 AND item_name = $2;";
 $result = pg_query_params($dbconn, $query, $params) or die("Query failed: " . pg_last_error());
 $row = pg_fetch_array($result);
 $highest_bid = $row['bid'];
+$highest_bid_int = $highest_bid;
 $next_highest_bid2 = $highest_bid+1;
 
 $params = array($owner, $item_name, $highest_bid);
@@ -103,153 +108,137 @@ if ($name) {
             </div>
             <div class="panel-body">
                 <div class="form-horizontal item-info">
-                  <div class="form-group">
-                     <div class="col-sm-4 form-group-content" align='center'>
-                        <a href="#" class="thumbnail">
-                            <img src="http://placehold.it/300x200" alt="...">
-                        </a>
-                    </div>
-                    <div class="col-sm-8">
-                        <div class="form-group">
-                            <label for="item_name" class="control-label">Item Name: </label>
-                            <div class="form-group-content">
-                                <?php echo $item_name ?>
+                    <div class="form-group">
+                        <div class="col-sm-4 form-group-content" align='center'>
+                            <a href="#" class="thumbnail">
+                                <img src="http://placehold.it/300x200" alt="...">
+                            </a>
+                        </div>
+                        <div class="col-sm-8">
+                            <div class="form-group">
+                                <label for="item_name" class="control-label">Item Name: </label>
+                                <div class="form-group-content">
+                                    <?php echo $item_name ?>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="owner_name" class="">Owner: </label>
+                                <div class="form-group-content"><?php echo $name ?></div>
                             </div>
                         </div>
-                        <div class="form-group">
-                            <label for="owner_name" class="">Owner: </label>
-                            <div class="form-group-content"><?php echo $name ?></div>
+                    </div>
+                    <div class="form-group">
+                        <label for="type" class="col-sm-3 control-label">Type: </label>
+                        <div class="col-sm-9 form-group-content">
+                            <?php echo $type ?>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="description" class="col-sm-3 control-label">Description: </label>
+                        <div class="col-sm-9 form-group-content">
+                            <?php echo $description ?>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="quantity" class="col-sm-3 control-label">Quantity: </label>
+                        <div class="col-sm-9 form-group-content">
+                            <?php echo $quantity ?>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="pickup_location" class="col-sm-3 control-label">Pickup Location: </label>
+                        <div class="col-sm-9 form-group-content">
+                            <?php echo $pickup_location ?>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="return_location" class="col-sm-3 control-label">Return Location: </label>
+                        <div class="col-sm-9 form-group-content">
+                            <?php echo $return_location ?>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="return-date" class="col-sm-3 control-label">Return Date: </label>
+                        <div class="col-sm-9 form-group-content">
+                            <?php echo $return_date ?>
+                        </div>
+                    </div>						
+                </div>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">Bidding Information</h3>
+            </div>
+            <div class="panel-body">
+                <div class="form-horizontal item-info">
+                    <div class="form-group">
+                        <label for="starting_bid" class="col-sm-3 control-label">Starting Bid: </label>
+                        <div class="col-sm-9 form-group-content">
+                            <?php echo $starting_bid ?>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="highest_bid" class="col-sm-3 control-label">Highest Bid: </label>
+                        <div class="col-sm-9 form-group-content">
+                            <?php echo $highest_bid; 
+                             if ($highest_bid != 'None') {
+                             ?>
+                             <label for="bidder">BY </label>
+                             <?php echo $bidder;
+                                    };
+                                    if ($highest_bid != 'None') {
+                                    ?>
+                                    <label for="created">ON </label>
+                                    <?php echo $created;
+                                           };
+                                           ?>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="buyout" class="col-sm-3 control-label">Buyout: </label>
+                        <div class="col-sm-9 form-group-content">
+                            <?php echo $buyout ?>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="bid_deadline" class="col-sm-3 control-label">Bid Deadline: </label>
+                        <div class="col-sm-9 form-group-content">
+                            <?php echo $bid_deadline ?>
                         </div>
                     </div>
                 </div>
-                <div class="form-group">
-                    <label for="type" class="col-sm-3 control-label">Type: </label>
-                    <div class="col-sm-9 form-group-content">
-                        <?php echo $type ?>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="description" class="col-sm-3 control-label">Description: </label>
-                    <div class="col-sm-9 form-group-content">
-                        <?php echo $description ?>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="quantity" class="col-sm-3 control-label">Quantity: </label>
-                    <div class="col-sm-9 form-group-content">
-                        <?php echo $quantity ?>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="pickup_location" class="col-sm-3 control-label">Pickup Location: </label>
-                    <div class="col-sm-9 form-group-content">
-                        <?php echo $pickup_location ?>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="return_location" class="col-sm-3 control-label">Return Location: </label>
-                    <div class="col-sm-9 form-group-content">
-                        <?php echo $return_location ?>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="return-date" class="col-sm-3 control-label">Return Date: </label>
-                    <div class="col-sm-9 form-group-content">
-                        <?php echo $return_date ?>
-                    </div>
-                </div>						
             </div>
         </div>
-    </div>
+
+        <?php if ($buyout_int != $highest_bid_int && isset($_SESSION['username']) && strtotime($bid_deadline) > time()) { ?>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">Place Your Bid</h3>
+            </div>
+            <div class="panel-body">
+                <form namd = "bidForm" action="bid.php" method="post">
+                    <div class="form-group">
+                        <label for="pickup-location" class="control-label">Your Bid</label>
+                        <input type="number" name='newBid' class="form-control" min = "<?php echo $next_highest_bid2 ?>" autofocus required> 
+                        <input type="hidden" name = "owner" value = "<?php echo $owner ?>">
+                        <input type="hidden" name = "itemName" value = "<?php echo $item_name ?>">
+                        <input type="hidden" name = "url" value = "<?php echo $_SERVER['REQUEST_URI'] ?>">
+                    </div>
+                    <button class="btn btn-default" name='bid-submit' type="submit">Submit</button>
+                </form>
+            </div>
+        </div>
+        <?php } ?>
     <?php 
     if (!$exists) {
         echo "<script>document.querySelector('.panel').remove();</script>";
         create_notification('danger', 'Item does not exist.');
     }
     ?>
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h3 class="panel-title">Bidding Information</h3>
-        </div>
-        <div class="panel-body">
-           <div class="form-horizontal item-info">
-               <div class="form-group">
-                <label for="starting_bid" class="col-sm-3 control-label">Starting Bid: </label>
-                <div class="col-sm-9 form-group-content">
-                    <?php echo $starting_bid ?>
-                </div>
-            </div>
-            <div class="form-group">
-             <label for="highest_bid" class="col-sm-3 control-label">Highest Bid: </label>
-             <div class="col-sm-9 form-group-content">
-                <?php echo $highest_bid; 
-                if ($highest_bid != 'None') {
-                    ?>
-                    <label for="bidder">BY </label>
-                    <?php echo $bidder;
-                };
-                if ($highest_bid != 'None') {
-                    ?>
-                    <label for="created">ON </label>
-                    <?php echo $created;
-                };
-                ?>
-            </div>
-        </div>
-        <div class="form-group">
-            <label for="buyout" class="col-sm-3 control-label">Buyout: </label>
-            <div class="col-sm-9 form-group-content">
-                <?php echo $buyout ?>
-            </div>
-        </div>
-        <div class="form-group">
-            <label for="bid_deadline" class="col-sm-3 control-label">Bid Deadline: </label>
-            <div class="col-sm-9 form-group-content">
-                <?php echo $bid_deadline ?>
-            </div>
-        </div>
-    </div>
-</div>
-</div>
-
-<?php 
-$show = '';
-if (isset($_SESSION['username'])){
-    echo "<p><i>You are logged in as ".
-    $_SESSION['username'] . "</i></p>";
-    $show = "display:show";
-} else {
-    echo "<p>You are not logged in.
-    Please login <a href='./login.php'>here</a> to place your bid.</p>";
-    $show = "display:none";
-}
-?>
-<div class="panel panel-default" style = <?php echo $show ?>>
-    <div class="panel-heading">
-        <h3 class="panel-title">Place Your Bid</h3>
-    </div>
-    <div class="panel-body">
-        <form namd = "bidForm" action="bid.php" method="post">
-            <div class="form-group">
-                <label for="pickup-location" class="control-label">Your Bid</label>
-                <input type="number" name='newBid' class="form-control" min = "<?php echo $next_highest_bid2 ?>" autofocus required> 
-                <input type="hidden" name = "owner" value = "<?php echo $owner ?>">
-                <input type="hidden" name = "itemName" value = "<?php echo $item_name ?>">
-                <input type="hidden" name = "url" value = "<?php echo $_SERVER['REQUEST_URI'] ?>">
-            </div>
-            <button class="btn btn-default" name='bid-submit' type="submit">Submit</button>
-        </form>
-    </div>
-</div>
-<?php 
-if (!$exists) {
-    echo "<script>document.querySelector('.panel').remove();</script>";
-    create_notification('danger', 'Item does not exist.');
-}
-?>
-</div>
-<?php
-pg_close($dbconn);
-?>
+    </div> <!-- content -->
+    <?php
+    pg_close($dbconn);
+    ?>
 </body>
 </html>
